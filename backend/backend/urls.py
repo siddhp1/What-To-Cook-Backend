70a628e3886476b7e685fbcc87eab4140e3988a8
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2024-07-11 16:55:35 -0400

    created new app for dishes with a dish model, serializer, and views to create and read

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,7 +4,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/dishes/', include('dishes.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
---
+++ backend/dishes/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Dish
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'image', 'cuisine', 'date_last_made', 'rating', 'time_to_make')
+    search_fields = ('name', 'user__email')
