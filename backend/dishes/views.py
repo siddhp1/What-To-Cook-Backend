32bc36bd75b4051f40c9e93241378ff3bc269453
siddhp1 <siddh.patel@outlook.com>
2024-07-16 14:45:22 -0400

    added pagination and search

--- backend/dishes/views.py
+++ backend/dishes/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.pagination import PageNumberPagination
 from .models import Dish
 from .serializers import DishSerializer
 
@@ -9,6 +10,8 @@ class DishViewSet(viewsets.ModelViewSet):
     serializer_class = DishSerializer
     parser_classes = (MultiPartParser, FormParser)
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'cuisine']
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
