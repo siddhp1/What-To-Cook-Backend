f91ee8dff16edd92959625fccf821128b597b346
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2024-07-17 18:49:21 -0400

    added ordering

--- backend/dishes/views.py
+++ backend/dishes/views.py
@@ -6,12 +6,13 @@ from .models import Dish
 from .serializers import DishSerializer
 
 class DishViewSet(viewsets.ModelViewSet):
-    queryset = Dish.objects.all()
+    queryset = Dish.objects.all().order_by('-date_last_made')
     serializer_class = DishSerializer
     parser_classes = (MultiPartParser, FormParser)
     permission_classes = [IsAuthenticated]
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['name', 'cuisine']
+    ordering_fields = ['date_last_made', 'cuisine', 'name']
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
