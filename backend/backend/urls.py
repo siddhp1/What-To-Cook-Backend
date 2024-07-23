10e62eee0564cf425f695760869b12a2c10a27d7
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2024-07-22 22:10:32 -0400

    added user id to token refresh serializer

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from users.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,14 +1,18 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, TokenObtainPairSerializer
 
 class UserCreateView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
     
-class UserDetailView(generics.RetrieveUpdateAPIView):
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+class TokenObtainPairView(TokenObtainPairView):
+    serializer_class = TokenObtainPairSerializer
