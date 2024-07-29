68338b163b0ab37dc4e11faae20d9f9b12670be0
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2024-07-29 14:50:34 -0400

    removed full image url field from serializer

--- backend/dishes/serializers.py
+++ backend/dishes/serializers.py
@@ -2,19 +2,11 @@ from rest_framework import serializers
 from .models import Dish
 
 class DishSerializer(serializers.ModelSerializer):
-    full_image_url = serializers.SerializerMethodField()
-
     class Meta:
         model = Dish
-        fields = ['id', 'name', 'image', 'cuisine', 'date_last_made', 'rating', 'time_to_make', 'full_image_url']
+        fields = ['id', 'name', 'image', 'cuisine', 'date_last_made', 'rating', 'time_to_make']
 
-    def get_full_image_url(self, obj):
-        request = self.context.get('request')
-        if obj.image and hasattr(obj.image, 'url'):
-            return request.build_absolute_uri(obj.image.url)
-        return None
-    
 class DishLimitedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dish
-        fields = ['id']  # Assuming you want to include all fields, adjust as necessary
+        fields = ['id']
